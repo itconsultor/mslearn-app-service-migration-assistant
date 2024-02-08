6603940fba9f2fa39cd0069a2fafe9ec85c53302
Carlos Tellez <carlos.cuervo@threatx.com>
2024-02-08 11:40:58 -0500

    Rolled back to 1.0.3 version for App service migration assistant

--- vm-setup.ps1
+++ vm-setup.ps1
@@ -7,14 +7,14 @@ mkdir C:\Exercise
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $wc = new-object System.Net.WebClient
 $wc.DownloadFile("https://github.com/MicrosoftDocs/mslearn-app-service-migration-assistant/releases/download/1.0/WebDeploy_amd64_en-US.msi", "C:\Exercise\WebDeploy_amd64_en-US.msi")
-$wc.DownloadFile("https://appmigration.microsoft.com/api/download/windows/AppServiceMigrationAssistant.msi", "C:\Exercise\AppServiceMigrationAssistant.msi")
+$wc.DownloadFile("https://github.com/MicrosoftDocs/mslearn-app-service-migration-assistant/releases/download/1.0/AppServiceMigrationAssistant.1.0.3.msi", "C:\Exercise\AppServiceMigrationAssistant.1.0.3.msi")
 $wc.DownloadFile("https://github.com/MicrosoftDocs/mslearn-app-service-migration-assistant/releases/download/1.0/partsunlimitedwebsite.zip", "C:\Exercise\partsunlimitedwebsite.zip")
 $wc.DownloadFile("https://github.com/MicrosoftDocs/mslearn-app-service-migration-assistant/releases/download/1.0/partsunlimitedwebsite.deploy.cmd", "C:\Exercise\partsunlimitedwebsite.deploy.cmd")
 $wc.DownloadFile("https://github.com/MicrosoftDocs/mslearn-app-service-migration-assistant/releases/download/1.0/partsunlimitedwebsite.SetParameters.xml", "C:\Exercise\partsunlimitedwebsite.SetParameters.xml")
 $wc.DownloadFile("https://raw.githubusercontent.com/MicrosoftDocs/mslearn-app-service-migration-assistant/master/setup.sql", "C:\Exercise\setup.sql")
 
 Start-Process "msiexec.exe" -ArgumentList '/I C:\Exercise\WebDeploy_amd64_en-US.msi /qn /norestart ADDLOCAL=ALL' -Wait -NoNewWindow
-Start-Process "msiexec.exe" -ArgumentList '/I "C:\Exercise\AppServiceMigrationAssistant.msi" /qn /norestart ALLUSERS=1' -Wait -NoNewWindow
+Start-Process "msiexec.exe" -ArgumentList '/I "C:\Exercise\AppServiceMigrationAssistant.1.0.3.msi" /qn /norestart ALLUSERS=1' -Wait -NoNewWindow
 
 $sql = get-content C:\Exercise\setup.sql
 $cn = new-object system.data.SqlClient.SQLConnection($connstr);
